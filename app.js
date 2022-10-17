fae4a74541960f3edf8d78a1e58d3c022ecc43c5
1145308 <sangeetakurmi@gmail.com>
2022-10-17 20:49:20 +0530

    Initial commit -2

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var UserName = prompt("Give me a UserName")
-alert("hello " + UserName)
+// var UserName = prompt("Give me a UserName")
+// alert("hello " + UserName)
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click" , clickHandler)
+
+function clickHandler() {
+
+console.log("clicked!");
+
+};
