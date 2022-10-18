7893f669749ca46144d1c0ca97e75634526226e9
1145308 <sangeetakurmi@gmail.com>
2022-10-18 22:45:20 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// var UserName = prompt("Give me a UserName")
-// alert("hello " + UserName)
-
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click" , clickHandler)
+
 
 function clickHandler() {
 
-console.log("clicked!");
+outputDiv.innerText = "ajabhbmmjabxjashcbjhvc " + txtInput.value;
 
 };
+btnTranslate.addEventListener("click" , clickHandler)
